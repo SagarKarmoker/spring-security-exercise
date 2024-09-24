0e139696bcc88a400c45b6685575b1b310997f63
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-25 01:07:04 +0600

    fixed: some routing

---
+++ src/main/java/com/sagar/springSecurityExample/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.sagar.springSecurityExample.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping
+    public String index() {
+        return "Hello World";
+    }
+
+    @GetMapping("/get-session")
+    public String getSession(HttpServletRequest request) {
+        return request.getSession().getId();
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken csrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
--- src/main/java/com/sagar/springSecurityExample/controller/StudentController.java
+++ src/main/java/com/sagar/springSecurityExample/controller/StudentController.java
@@ -2,34 +2,17 @@ package com.sagar.springSecurityExample.controller;
 
 import com.sagar.springSecurityExample.model.Student;
 import com.sagar.springSecurityExample.service.StudentService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/")
-public class DemoController {
+public class StudentController {
 
     @Autowired
     private StudentService studentService;
-
-    @GetMapping
-    public String index() {
-        return "Hello World";
-    }
-
-    @GetMapping("/get-session")
-    public String getSession(HttpServletRequest request) {
-        return request.getSession().getId();
-    }
-
-    @GetMapping("/csrf-token")
-    public CsrfToken csrfToken(HttpServletRequest request) {
-        return (CsrfToken) request.getAttribute("_csrf");
-    }
 
     @GetMapping("/students")
     public List<Student> students() {
