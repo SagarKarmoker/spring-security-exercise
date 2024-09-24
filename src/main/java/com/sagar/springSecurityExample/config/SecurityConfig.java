d639a7671bbcfd8067685b32e10dcfde66108499
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-25 01:26:20 +0600

    upto: user custom login route and auth using username and password

--- src/main/java/com/sagar/springSecurityExample/config/SecurityConfig.java
+++ src/main/java/com/sagar/springSecurityExample/config/SecurityConfig.java
@@ -4,9 +4,11 @@ package com.sagar.springSecurityExample.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,7 +30,10 @@ public class SecurityConfig {
 
         // builder pattern
         http.csrf(customizer -> customizer.disable()) // disable the csrf token
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated()) // route must be authenticated
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("register", "login")
+                        .permitAll()
+                        .anyRequest().authenticated()) // route must be authenticated
                 //http.formLogin(Customizer.withDefaults()); // enable form login
                 .httpBasic(Customizer.withDefaults()) // for REST Api
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // session id everytime (browser)
@@ -68,5 +73,10 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(new BCryptPasswordEncoder(10));
         authProvider.setUserDetailsService(userDetailsService);
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/sagar/springSecurityExample/service/UserService.java
+++ src/main/java/com/sagar/springSecurityExample/service/UserService.java
@@ -3,7 +3,9 @@ package com.sagar.springSecurityExample.service;
 import com.sagar.springSecurityExample.model.Users;
 import com.sagar.springSecurityExample.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,9 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     // place here or at security config
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10); // round = 10
 
@@ -20,5 +25,15 @@ public class UserService {
         user.setPassword(encoder.encode(user.getPassword()));
         userRepo.save(user);
         return user;
+    }
+
+    public String verify(Users user) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return "success";
+        }
+        return "fail";
     }
 }
